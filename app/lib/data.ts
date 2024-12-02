ff87f82a3072cf18ca539f8ff2933014c6253d55
Javier <javiermvm6@gmail.com>
2024-12-01 18:27:45 -0600

    yeet

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,33 +1,24 @@
 
 import {FormattedDoctoresTable, FormattedHospitalesTable} from './definitions';
+import axios from "axios";
 //TODO arreglar la db para poder pedir todo
 export async function fetchDoctoresInfo() {
+    const url='http://localhost:3010/api/doctores'
+    const response = await fetch(url);
+    if(!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    return data;
 
-  const url = 'http://localhost:3010/api/doctores';
-  const data = await fetch(url);
-
-  // var doctores: FormattedDoctoresTable[] = [];
-  // var doctor: FormattedDoctoresTable = {
-  //   id: 1,
-  //   idEspecializacion:1,
-  //   nombre:"caesar",
-  //   idDepartamento:1,
-  //   idHospital:1
-  // }
-  // doctores.push(doctor);
-  // var doctor: FormattedDoctoresTable = {
-  //   id: 2,
-  //   idEspecializacion:12,
-  //   nombre:"Javier",
-  //   idDepartamento:12,
-  //   idHospital:13
-  // }
-  // doctores.push(doctor);
-  return doctores;
 }
 export async function fetchHospitales(){
   const url='http://localhost:3010/api/hospitales';
-  var hospitales: FormattedHospitalesTable[] = [];
+  const response = await fetch(url);
+  if(!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const hospitales = await response.json();
   return hospitales;
 }
 
