4b4713271bc0599ac9cb35f03c847c3b79c3d445
Javier <javiermvm6@gmail.com>
2024-12-06 13:19:03 -0600

    putas comillas

--- app/dashboard/medicina/[id]/details/page.tsx
+++ app/dashboard/medicina/[id]/details/page.tsx
@@ -7,12 +7,12 @@ export default async function DetailsPage(props : {params: Promise<{id : number}
     const params = await props.params;
     const id = params.id;
     const data = await fetchMedicinaById(id);
-//supadre
+
     return(
         <main>
             <div className={'text-4xl'}><Breadcrumbs breadcrumbs={[
                 {label: 'Medicinas', href: '/dashboard/medicina'},
-                {label: 'KaisoCida', href: '/dashboard/medicina/id/details, active : true'},
+                {label: 'KaisoCida', href: `/dashboard/medicina/${id}/details`, active : true},
             ]}/></div>
             <h1 className={'text-4xl  font-bold mb-4'}>${data.NOMBRE_MEDICINA}</h1>
             <div className=" p-3 bg-gray-100 rounded-xl text-2xl">
