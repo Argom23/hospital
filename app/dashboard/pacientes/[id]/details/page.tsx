0253ec2fe92acf04ac9af5c93f6b007f4906ba1e
Javier <javiermvm6@gmail.com>
2024-12-09 18:48:25 -0600

    CEASE YOUR RESISTANCE

--- app/dashboard/pacientes/[id]/details/page.tsx
+++ app/dashboard/pacientes/[id]/details/page.tsx
@@ -1,5 +1,39 @@
+
+import Breadcrumbs from "@/app/ui/breadcrumbs";
+import Link from "next/link";
+import {fetchPacientesById, fetchPersonalById} from "@/app/lib/data";
+
 export default async function Page(props : {params: Promise<{id : number}>}){
     const params = await props.params;
     const id = params.id;
-    return <h1>La hostia</h1>;
+    const data = await fetchPacientesById(id);
+    return(
+        <main>
+            <div>
+                <Breadcrumbs breadcrumbs={[
+                    {label: "Pacientes", href: "/dashboard/pacientes"},
+                    {label: ` ${data[0].NOMBRE_COMPLETO}`, href: `/dashboard/pacientes/${id}/details`, active:true},
+                ]}/>
+            </div>
+            <div className=" p-3 bg-gray-100 rounded-xl text-xl mb-1">
+
+                <div>
+                    <label className={'mt-1'}>Numero: {data[0].NUMERO_PACIENTE}</label>
+                </div>
+                <div>
+                    <label className={'mt-1'}>Correo: {data[0].CORREO_PACIENTE}</label>
+                </div>
+                <div>
+                    <label className={'mt-1'}>Direccion: {data[0].DIRECCION}</label>
+                </div>
+
+            </div>
+            <div className=" mt-4 flex  gap-4 ">
+                <Link href="/dashboard/personal"
+                      className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-base font-medium text-white transition-colors hover:bg-blue-400 duration-300">
+                    Volver
+                </Link>
+            </div>
+        </main>
+    );
 }
--- app/dashboard/receta/[id]/details/page.tsx
+++ app/dashboard/receta/[id]/details/page.tsx
@@ -1,5 +1,38 @@
+import {fetchMedicina, fetchMedicinaById, fetchPacientesById, fetchRecetasById} from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/breadcrumbs";
+import Link from "next/link";
+
 export default async function Page(props : {params: Promise<{id : number}>}){
     const params = await props.params;
     const id = params.id;
-    return <h1>La hostia</h1>;
+    const data = await fetchRecetasById(id);
+    const medicina = await fetchMedicinaById(data[0].ID_MEDICINA);
+    const paciente = await fetchPacientesById(data[0].ID_PACIENTE);
+    return(
+        <main>
+            <div>
+                <Breadcrumbs breadcrumbs={[
+                    {label: "Recetas", href: "/dashboard/receta"},
+                    {label: `Receta Num: ${data[0].ID_RECETA}`, href: `/dashboard/receta/${id}/details`, active:true},
+                ]}/>
+            </div>
+            <div className=" p-3 bg-gray-100 rounded-xl text-xl mb-1">
+                <div>
+                    <label className={'mt-1'}>Paciente: {paciente[0].NOMBRE_PACIENTE}</label>
+                </div>
+                <div>
+                    <label className={'mt-1'}>Medicamento: {medicina[0].NOMBRE_MEDICINA}</label>
+                </div>
+                <div>
+                    <label className={'mt-1'}>Fecha: {data[0].FECHA_RECETA}</label>
+                </div>
+            </div>
+            <div className=" mt-4 flex  gap-4 ">
+                <Link href="/dashboard/receta"
+                      className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-base font-medium text-white transition-colors hover:bg-blue-400 duration-300">
+                    Volver
+                </Link>
+            </div>
+        </main>
+    );
 }
